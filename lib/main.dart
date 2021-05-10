8e4d903d02886316f5e91e9a3e68176c4296dfcd
KhineKhineLin <santun2811@gmail.com>
2021-05-11 03:33:06 +0700

    test branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//create branch khinelin
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -25,88 +27,93 @@ class _MainPageState extends State<MainPage> {
       body: Stack(
         children: [
           Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: MediaQuery.of(context).size.height/2.2,
-            child: Container(
-              padding: EdgeInsets.only(left: 16,right: 16,top: 38,bottom: 32),
-              decoration: BoxDecoration(
-                
-                color: Colors.red,
-                image: DecorationImage(
-                  colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
-                  image: NetworkImage('https://cdn.pixabay.com/photo/2014/09/26/09/33/girls-462072_960_720.jpg'),
-                  fit: BoxFit.cover
-                )
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.menu,color: Colors.white,),
-                      Icon(Icons.add_shopping_cart,color: Colors.white,)
-                    ],
-                  ),
-                  
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 4,
-                        width: 16,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(8)
-                        ),
-                      ),
-                      SizedBox(width: 4,),
-                      Container(
-                        height: 4,
-                        width: 4,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(8)
-                        ),
-                      ),
-                      SizedBox(width: 4,),
-                      Container(
-                        height: 4,
-                        width: 4,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(8)
-                        ),
-                      ),
-                      SizedBox(width: 4,),
-                      Container(
-                        height: 4,
-                        width: 4,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(8)
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )),
-            Positioned(
               left: 0,
               right: 0,
-              top: MediaQuery.of(context).size.height/2.55,
+              top: 0,
+              bottom: MediaQuery.of(context).size.height / 2.2,
+              child: Container(
+                padding:
+                    EdgeInsets.only(left: 16, right: 16, top: 38, bottom: 32),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.5), BlendMode.darken),
+                        image: NetworkImage(
+                            'https://cdn.pixabay.com/photo/2014/09/26/09/33/girls-462072_960_720.jpg'),
+                        fit: BoxFit.cover)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.add_shopping_cart,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 4,
+                          width: 16,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 4,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 4,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 4,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(8)),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              top: MediaQuery.of(context).size.height / 2.55,
               bottom: 0,
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    
-                  )
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                    )),
                 padding: EdgeInsets.only(top: 16),
                 child: Column(
                   children: [
@@ -115,27 +122,33 @@ class _MainPageState extends State<MainPage> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text('Recommend',style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
-                          ),),
-                          Text('See All >',style: TextStyle(
-                            color: Colors.grey
-                          ),)
+                          Text(
+                            'Recommend',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                          Text(
+                            'See All >',
+                            style: TextStyle(color: Colors.grey),
+                          )
                         ],
                       ),
                     ),
-                    SizedBox(height: 16,),
+                    SizedBox(
+                      height: 16,
+                    ),
                     Container(
                       height: 48,
                       child: Placeholder(),
                     ),
-                    SizedBox(height: 16,),
+                    SizedBox(
+                      height: 16,
+                    ),
                     Container(
                       height: 264,
-                      child: Placeholder(
-                      ),
+                      child: Placeholder(),
                     )
                   ],
                 ),
@@ -145,7 +158,17 @@ class _MainPageState extends State<MainPage> {
       bottomNavigationBar: BottomAppBar(
         elevation: 12,
         child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
           height: 64,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(icon: Icon(Icons.home), onPressed: (){}),
+              IconButton(icon: Icon(Icons.favorite), onPressed: (){}),
+              IconButton(icon: Icon(Icons.card_giftcard), onPressed: (){}),
+              IconButton(icon: Icon(Icons.account_circle), onPressed: (){})
+            ],
+          ),
         ),
       ),
     );
